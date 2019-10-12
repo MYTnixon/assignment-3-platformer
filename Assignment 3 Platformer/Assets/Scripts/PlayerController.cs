9a1ec621e5de5678ce43ab4dac26ada98cd2cdac
nickcampoli <dacuel21@gmail.com>
2019-10-12 03:32:16 -0500

    Got started on player movement

---
+++ Assignment 3 Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rbody;
+    SpriteRenderer spriteRenderer;
+    bool isGrounded;
+
+    [SerializeField]
+    Transform platformCheck;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics2D.Linecast(transform.position, platformCheck.position, 1 << LayerMask.NameToLayer("Platforms")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rbody.velocity = new Vector2(8, rbody.velocity.y);
+            //animator.Play("Player_Run");
+            spriteRenderer.flipX = false;
+        }
+        else if (Input.GetKey("a"))
+        {
+            rbody.velocity = new Vector2(-8, rbody.velocity.y);
+            //animator.Play("Player_Run");
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            //animator.Play("Player_Idle");
+            rbody.velocity = new Vector2(0, rbody.velocity.y);
+        }
+
+        if (Input.GetKey("space") && isGrounded)
+        {
+            rbody.velocity = new Vector3(rbody.velocity.x, 3);
+            animator.Play("Player_Jump");
+        }
+    }
+}
