700b74f78d9068f0b19263e9e1b93fd2ac6e858e
nickcampoli <dacuel21@gmail.com>
2019-10-16 17:08:25 -0500

    Added enemy patrol

---
+++ Assignment 3 Platformer/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+    private bool movingRight = true;
+    public Transform enemyPlatformCheck;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D platform = Physics2D.Raycast(enemyPlatformCheck.position, Vector2.down, distance);
+        if (platform.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }
+}
